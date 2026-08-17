0049c80e68e50701465d4616f74cd4c55a721f70
Mehmet CAKMAK <cakmakm541@gmail.com>
2026-08-17 14:04:12 +0300

    Two-Pahase Commit 2PC example intialized

--- 1.CommunicationBetweenMicroservices/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ 1.CommunicationBetweenMicroservices/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d14af6a63c88edda863f84661c3b844575f5007c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631f07c685e9342244eda61620780f73cde9405e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
