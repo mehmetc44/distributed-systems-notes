e9b3476b794c29c79429a26db64b16190bc1a152
Mehmet CAKMAK <cakmakm541@gmail.com>
2026-08-18 16:43:48 +0300

    Bugs fixed

--- 3.EventualConsistencySagaChoreography/Stock.API/Program.cs
+++ 3.EventualConsistencySagaChoreography/Stock.API/Program.cs
@@ -33,13 +33,21 @@ var app = builder.Build();
 using IServiceScope scope = app.Services.CreateScope();
 MongoDBService mongoDbService = scope.ServiceProvider.GetRequiredService<MongoDBService>();
 var stockCollection = mongoDbService.GetCollection<Stock.API.Models.Stock>();
-if (!stockCollection.Find(_ => true).Any())
+if (!await stockCollection.Find(_ => true).AnyAsync())
 {
+    Console.WriteLine("MongoDB'ye örnek stok verileri ekleniyor...");
     await stockCollection.InsertOneAsync(new() { ProductId = Guid.NewGuid(), Count = 100 });
     await stockCollection.InsertOneAsync(new() { ProductId = Guid.NewGuid(), Count = 200 });
     await stockCollection.InsertOneAsync(new() { ProductId = Guid.NewGuid(), Count = 50 });
     await stockCollection.InsertOneAsync(new() { ProductId = Guid.NewGuid(), Count = 30 });
     await stockCollection.InsertOneAsync(new() { ProductId = Guid.NewGuid(), Count = 5 });
+}
+
+var stocks = await stockCollection.Find(_ => true).ToListAsync();
+Console.WriteLine($"MongoDB Stok Sayısı: {stocks.Count}");
+foreach (var s in stocks)
+{
+    Console.WriteLine($"[Stock] ProductId: {s.ProductId} | Count: {s.Count}");
 }
 
 
